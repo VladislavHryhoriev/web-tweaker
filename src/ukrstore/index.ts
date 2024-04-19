e0afc2c7313eee70d274bd3fc900606ee52443d3
Hryhoriev Vladislav <wladislav101@gmail.com>
2024-04-19 18:01:15 +0300

    some updates

--- src/ukrstore/index.ts
+++ src/ukrstore/index.ts
@@ -1,16 +1,28 @@
 import createButton from '../utils/create/createButton';
-import { $, $children, $closest } from '../utils/selectors';
-import { selectForbbidenButton, tireModButton } from './ui/buttons';
+import { $, $children } from '../utils/selectors';
+import { selectForbbidenButton } from './ui/buttons';
 import searchId from './utils/searchId';
 import waitLoading from './utils/waitLoading';
 
-document.body.addEventListener('click', () => {
-	const node = $closest('.tab a[data-tab="3"]', '.active');
-	const btnNode = $('.tire-moderate');
-	// const menu = $('.form-inline');
+const tabs = {
+	_activeTab: 0,
 
-	if (node) {
-		// menu.append(tireModButton, selectForbbidenButton);
+	get current() {
+		return this._activeTab;
+	},
+
+	set setCurrent(value: string | null | undefined) {
+		this._activeTab = Number(value);
+	},
+};
+
+document.body.addEventListener('click', () => {
+	const activeTab = $(`li.tab.active`).firstElementChild;
+	tabs.setCurrent = activeTab?.getAttribute('data-tab');
+
+	if (tabs.current === 3) {
+		const menu = $('.form-inline');
+		menu.append(selectForbbidenButton);
 
 		waitLoading(() => {
 			const list = [...$children('.j-list-body')];
@@ -19,26 +31,25 @@ document.body.addEventListener('click', () => {
 					'.l-table-actions'
 				) as HTMLElement;
 
-				const currentNodeId = currentNode?.children[0]?.getAttribute('onclick');
+				const currentNodeId = currentNode?.children[0]
+					?.getAttribute('onclick')
+					?.match(/\d+/);
+
 				if (currentNodeId) {
-					const matchedId = currentNodeId.match(/\d+/);
-					if (matchedId) {
-						const currentId = matchedId[0];
-						const newButton = createButton({
-							title: currentId,
-							className: 'search-id',
-							handler: () => searchId(currentId),
-							buttonType: 'blue',
-						});
-						currentNode.prepend(newButton);
-					}
+					const currentId = currentNodeId[0];
+
+					const newButton = createButton({
+						title: currentId,
+						className: 'search-id',
+						handler: () => searchId(currentId),
+						buttonType: 'blue',
+					});
+					currentNode.prepend(newButton);
 				}
 			});
 		});
 	} else {
-		if (btnNode) {
-			$('.form-inline').removeChild(tireModButton);
+		if ($('.selectForbbidenButton'))
 			$('.form-inline').removeChild(selectForbbidenButton);
-		}
 	}
 });
--- src/ukrstore/ui/buttons.ts
+++ src/ukrstore/ui/buttons.ts
@@ -1,41 +1,44 @@
 import createButton from '../../utils/create/createButton';
 import { $ } from '../../utils/selectors';
 import { getList } from '../utils/getList';
-import searchTitle from '../utils/searchTitle';
 import waitLoading from '../utils/waitLoading';
 
 const forbbidenWords = [
 	'Приглашаем на работу в салон Vip-уровня массажисток',
 	'Открыт набор в Элитное эскорт-сопровождении агентство Киева',
+	'Высокооплачиваемая работа для девушек в сфере досуга',
+	'Высокооплачиваемая работа',
 	'cas',
+	'интим',
+	'эскорт',
+	'сигареты',
+	'секс',
+	'iqos',
 ];
-
-const tireModerate = () => {
-	waitLoading(() => {
-		searchTitle('шина');
-	});
-};
+// j-mass-actions-bl c-actions-panel alert alert-warning
+// j-mass-actions-bl c-actions-panel alert alert-warning hide
 
 const selectForbbiden = async () => {
+	resetButton.click();
+
 	waitLoading(() => {
-		resetButton.click();
+		const list = getList();
 
-		waitLoading(() => {
-			const list = getList();
+		$('.c-actions-panel').classList.toggle('hide');
 
-			list.map((node) => {
-				const titleNode = node.querySelector('.c-link-ajax') as HTMLElement;
-				const title = titleNode.textContent?.toLowerCase();
+		list.map((node) => {
+			const titleNode = node.querySelector('.c-link-ajax') as HTMLElement;
+			const title = titleNode.textContent?.toLowerCase();
+			const checkWords = forbbidenWords.some((word) =>
+				title?.includes(word.toLowerCase())
+			);
 
-				if (
-					forbbidenWords.some((word) => title?.includes(word.toLowerCase()))
-				) {
-					const checkbox = node.querySelector(
-						'.check.j-mass-check-item'
-					) as HTMLInputElement;
-					checkbox.checked = true;
-				}
-			});
+			if (checkWords) {
+				const checkbox = node.querySelector(
+					'.check.j-mass-check-item'
+				) as HTMLInputElement;
+				checkbox.checked = true;
+			}
 		});
 	});
 };
@@ -43,16 +46,9 @@ const selectForbbiden = async () => {
 export const resetButton = $('[data-title="Сбросить"]');
 export const searchButton = $('input[value="Найти"]');
 
-export const tireModButton = createButton({
-	title: 'Модерация шин',
-	className: 'tire-moderate',
-	handler: tireModerate,
-	buttonType: 'blue',
-});
-
 export const selectForbbidenButton = createButton({
 	title: 'Выбрать запрещенное',
-	className: 'tire-moderate',
+	className: 'selectForbbidenButton',
 	handler: selectForbbiden,
 	buttonType: 'blue',
 });

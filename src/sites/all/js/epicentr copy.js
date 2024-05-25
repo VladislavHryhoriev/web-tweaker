e346cdef9850105b9d76b22d8b4e5a3e8fb39e06
Hryhoriev Vladislav <wladislav101@gmail.com>
2024-05-25 12:19:38 +0300

    update

--- src/sites/all/js/epicentr copy.js
+++ src/sites/all/js/epicentr copy.js
@@ -1,30 +1,21 @@
-const $all = (selector) => document.querySelectorAll(selector);
-const $$ = (selector) => document.querySelector(selector);
-const $text = (selector) => document.querySelector(selector)?.innerText;
-const $value = (selector) => document.querySelector(selector).value;
-const $style = (selector) => document.querySelector(selector).style;
-
-const waitForElement = (selector, callback) => {
-	const intervalId = setInterval(() => {
-		const nodeList = $all(selector);
-		const nodeArr = Array.from(nodeList);
-
-		if (nodeArr.length) {
-			clearInterval(intervalId);
-			callback(nodeArr);
-		}
-	}, 3000);
-};
-
-waitForElement('[type="number"]', (nodeArr) => {
-	nodeArr.map((node) => (node.readOnly = false));
-	console.log(
-		'readonly deactive for:\n' + nodeArr.map((node) => node.outerHTML + '\n')
-	);
+const $all = (e) => document.querySelectorAll(e),
+	$$ = (e) => document.querySelector(e),
+	$text = (e) => document.querySelector(e)?.innerText,
+	$value = (e) => document.querySelector(e).value,
+	$style = (e) => document.querySelector(e).style,
+	waitForElement = (e, o) => {
+		let l = setInterval(() => {
+			let n = $all(e),
+				a = Array.from(n);
+			a.length && (clearInterval(l), o(a));
+		}, 3e3);
+	};
+waitForElement('[type="number"]', (e) => {
+	e.map((e) => (e.readOnly = !1)),
+		console.log('readonly deactive for:\n' + e.map((e) => e.outerHTML + '\n'));
 });
-
-const createButton = (text, className, handler) => {
-	const initialStyles = `
+const createButton = (e, o, l) => {
+		let n = `
     display: block;
     position: relative;
     background-color: #0849b2;
@@ -34,148 +25,118 @@ const createButton = (text, className, handler) => {
     padding: 0.5em 1.5em;
     border: 1px solid #000;
     border-radius: 5px;
-  `;
-
-	const button = document.createElement('button');
-	button.textContent = text;
-	button.style = initialStyles;
-	button.classList.add(className);
-	button.addEventListener('click', handler);
-	button.addEventListener('mouseover', (e) =>
-		onHandlerStyles(e, `background-color: #02218a;`)
-	);
-	button.addEventListener('mouseout', (e) =>
-		onHandlerStyles(e, `background-color: #0849b2;`)
-	);
-
-	return button;
-};
-
-const onHandlerStyles = ({ target }, styles) => {
-	target.style.cssText += styles;
-	target.removeEventListener('mouseout', onHandlerStyles);
-};
-
-const copyData = ({ orderId, client, delivery, products, total }) => {
-	let text = '';
-
-	if ($text('.order-status').includes('Нове')) {
-		text = `
+  `,
+			a = document.createElement('button');
+		return (
+			(a.textContent = e),
+			(a.style = n),
+			a.classList.add(o),
+			a.addEventListener('click', l),
+			a.addEventListener('mouseover', (e) =>
+				onHandlerStyles(e, 'background-color: #02218a;')
+			),
+			a.addEventListener('mouseout', (e) =>
+				onHandlerStyles(e, 'background-color: #0849b2;')
+			),
+			a
+		);
+	},
+	onHandlerStyles = ({ target: e }, o) => {
+		(e.style.cssText += o), e.removeEventListener('mouseout', onHandlerStyles);
+	},
+	copyData = ({ orderId: e, client: o, delivery: l, products: n, total: a }) => {
+		let i = '';
+		(i = $text('.order-status').includes('Нове')
+			? `
 		Епіцентр
-		● ${orderId}
-		● ${products.map(({ article, amount }) => `${article} ${amount}`)} = ${total}`
-			.replace(/\s+/g, ' ')
-			.trim();
-	} else {
-		text = `
+		● ${e}
+		● ${n.map(({ article: e, amount: o }) => `${e} ${o}`)} = ${a}`
+					.replace(/\s+/g, ' ')
+					.trim()
+			: `
 		Епіцентр
-		● ${orderId}
-		● ${client?.lastName} ${client?.firstName} ${client?.patronymic}
-		● ${client?.phone}
-		● ${delivery?.provider}
-		● ${delivery?.city}
-		● ${delivery?.warehouse}
-		>> ${delivery?.note}`
-			.replace(/\s+/g, ' ')
-			.trim();
-	}
-
-	navigator.clipboard.writeText(text);
-};
-const t = new Date(1716623937023).getTime();
-
-const getData = () => {
-	try {
-		const products = [];
-
-		for (const node of [...$all('.order-products-list > *')]) {
-			const inputSelector =
-				node.querySelector('input[type="number"]')?.value ||
-				parseInt(
-					node.querySelector('.number-change').textContent.replace(/[^\d]/g, '')
-				);
-			products.push({
-				article: node.querySelector('.item-sku').innerText.split(' ')[1],
-				amount: inputSelector + 'шт',
-				price: node.querySelector('.item-amount').innerText.split(' ')[0],
-			});
+		● ${e}
+		● ${o?.lastName} ${o?.firstName} ${o?.patronymic}
+		● ${o?.phone}
+		● ${l?.provider}
+		● ${l?.city}
+		● ${l?.warehouse}
+		>> ${l?.note}`
+					.replace(/\s+/g, ' ')
+					.trim()),
+			navigator.clipboard.writeText(i);
+	},
+	t = new Date(1716623937023).getTime(),
+	getData = () => {
+		try {
+			let e = [];
+			for (let o of [...$all('.order-products-list > *')]) {
+				let l =
+					o.querySelector('input[type="number"]')?.value ||
+					parseInt(
+						o.querySelector('.number-change').textContent.replace(/[^\d]/g, '')
+					);
+				e.push({
+					article: o.querySelector('.item-sku').innerText.split(' ')[1],
+					amount: l + 'шт',
+					price: o.querySelector('.item-amount').innerText.split(' ')[0],
+				});
+			}
+			let n = {
+				orderId: $text('.order-title').split(' ')[1],
+				products: e,
+				total: $text('.total').split(' ')[3],
+				client: {
+					firstName: $value('input[formcontrolname="firstName"]'),
+					lastName: $value('input[formcontrolname="lastName"]'),
+					patronymic: $value('input[formcontrolname="patronymic"]'),
+					phone: $value('input[formcontrolname="phone"]')
+						.replace('+38', '')
+						.replace(/[^\d]/g, ''),
+					email: $value('input[formcontrolname="email"]'),
+				},
+				delivery: {
+					provider: $text('.selected-provider'),
+					city: $value(
+						'em-delivery-providers-select-city[formcontrolname="settlement"] input'
+					),
+					warehouse: $value(
+						'em-delivery-providers-select-office[formcontrolname="office"] textarea'
+					),
+					ttn: $value('input[formcontrolname="number"]').split(' ').join(''),
+					note: $text('.commentary-field > div'),
+				},
+			};
+			return n;
+		} catch (a) {
+			alert('Ошибка, детали в консоли'), console.error(a);
 		}
-
-		const form = {
-			orderId: $text('.order-title').split(' ')[1],
-			products: products,
-			total: $text('.total').split(' ')[3],
-			client: {
-				firstName: $value('input[formcontrolname="firstName"]'),
-				lastName: $value('input[formcontrolname="lastName"]'),
-				patronymic: $value('input[formcontrolname="patronymic"]'),
-				phone: $value('input[formcontrolname="phone"]')
-					.replace('+38', '')
-					.replace(/[^\d]/g, ''),
-				email: $value('input[formcontrolname="email"]'),
-			},
-			delivery: {
-				provider: $text('.selected-provider'),
-				city: $value(
-					'em-delivery-providers-select-city[formcontrolname="settlement"] input'
-				),
-				warehouse: $value(
-					'em-delivery-providers-select-office[formcontrolname="office"] textarea'
-				),
-				ttn: $value('input[formcontrolname="number"]').split(' ').join(''),
-				note: $text('.commentary-field > div'),
-			},
-		};
-
-		return form;
-	} catch (e) {
-		alert('Ошибка, детали в консоли');
-		console.error(e);
-	}
-};
-const r = new Date('2024-06-5').getTime();
-
-const checkUrl = () => {
-	const url = location.href;
-
-	if ($text('.order-status') === 'Нове') {
-		$$('.copy-info').textContent = 'Проверить товар';
-	} else {
-		$$('.copy-info').textContent = 'Скопировать для 1c';
-	}
-
-	if (url.includes('orders') && url.split('/').length > 5) {
-		$style('.private-buttons-menu').display = 'flex';
-	} else {
-		$style('.private-buttons-menu').display = 'none';
-	}
-};
-
-const copyToBuffer = () => copyData(getData());
-
-if (r > t) {
+	},
+	r = new Date('2024-06-5').getTime(),
+	checkUrl = () => {
+		let e = location.href;
+		'Нове' === $text('.order-status')
+			? ($$('.copy-info').textContent = 'Проверить товар')
+			: ($$('.copy-info').textContent = 'Скопировать для 1c'),
+			e.includes('orders') && e.split('/').length > 5
+				? ($style('.private-buttons-menu').display = 'flex')
+				: ($style('.private-buttons-menu').display = 'none');
+	},
+	copyToBuffer = () => copyData(getData());
+if (r > t)
 	window.onload = () => {
-		const menuStyles = `
+		let e = `
 		display: flex;
 		position: absolute;
 		left: calc(50% - (177px / 2));
 		bottom: 10px;
-	`;
-
-		const menu = document.createElement('div');
-		menu.classList.add('private-buttons-menu');
-		menu.style = menuStyles;
-
-		const copyBtn = createButton('Скопировать для 1c', 'copy-info', copyToBuffer);
-		// const getTTNBtn = createButton('Получить данные из ТТН', 'ttn-info', () => getTTNData(getData()));
-
-		document.body.append(menu);
-		menu.append(copyBtn); // getTTNBtn
-
-		checkUrl();
-
-		window.addEventListener('mousemove', checkUrl);
+	`,
+			o = document.createElement('div');
+		o.classList.add('private-buttons-menu'), (o.style = e);
+		let l = createButton('Скопировать для 1c', 'copy-info', copyToBuffer);
+		document.body.append(o),
+			o.append(l),
+			checkUrl(),
+			window.addEventListener('mousemove', checkUrl);
 	};
-} else {
-	throw Error('update page');
-}
+else throw Error('update page');
